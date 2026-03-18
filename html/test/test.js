c0bf6145045f020b9771159d46af253a1c4e5ec4
pperche <perche.paolo@gmail.com>
2026-03-18 13:51:38 +0100

    Add initial HTML and JavaScript structure for type effectiveness functionality

---
+++ html/test/test.js
@@ -0,0 +1,27 @@
+function getPokemonsByType(typeName){
+
+}
+
+function getPokemonsByAttack(attackName){
+    
+}
+
+function getAttackByType(typeName){
+    
+}
+
+function sortPokemonByTypeThenName(){
+    
+}
+
+function getWeakestEnemies(attackName){
+    
+}
+
+function getBestFastAttacksForEnemy(print, pokemonName){
+    
+}
+
+function fastFight(pokemonNameA, pokemonNameB){
+
+}
