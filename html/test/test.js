54ec62562077455588351089ff4af57a7abb46dd
pperche <perche.paolo@gmail.com>
2026-03-18 14:26:04 +0100

    Fix type checks and improve logging in Pokemon retrieval functions

--- html/test/test.js
+++ html/test/test.js
@@ -1,15 +1,15 @@
 function getPokemonsByType(typeName){
     Object.values(Pokemon.all_pokemons).forEach(element => {
-        if(element.type == typeName){
-            console.log(element);
+        if(element.types && element.types.includes(typeName)){
+            console.log(element.toString());
         }
     });
 }
 
 function getPokemonsByAttack(attackName){
     Object.values(Pokemon.all_pokemons).forEach(element => {
-        if(element.fast_attacks === attackName || element.charged_attacks === attackName){
-            console.log(element);
+        if(element.fastMoves && element.fastMoves.includes(attackName) || element.chargedMoves && element.chargedMoves.includes(attackName)){
+            console.log(element.toString());
         }
     });
 }
@@ -17,7 +17,7 @@ function getPokemonsByAttack(attackName){
 function getAttackByType(typeName){
     Object.values(Attack.all_attacks).forEach(element => {
         if(element.type == typeName){
-            console.log(element);
+            console.log(element.toString());
         }
     });
 }
@@ -39,5 +39,5 @@ function fastFight(pokemonNameA, pokemonNameB){
 }
 
 getPokemonsByType("Fire");
-getPokemonsByAttack("Fire Blast");
+getPokemonsByAttack("Flame Burst");
 getAttackByType("Fire");
